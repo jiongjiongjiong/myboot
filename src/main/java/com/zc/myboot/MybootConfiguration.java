f4467ecb3d31500c045dc2de28f9f715edeb863f
zc <cong.2036@gmail.com>
2017-11-23 00:55:17 +0800

    修改静态文件地址

--- src/main/java/com/zc/myboot/MybootConfiguration.java
+++ src/main/java/com/zc/myboot/MybootConfiguration.java
@@ -5,16 +5,24 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 /**
  * @author: zc
- * @date: 2017/11/19
+ * @date: 2017/11/22
  */
 @Configuration
-public class FastJsonConfiguration  extends WebMvcConfigurerAdapter{
+public class MybootConfiguration extends WebMvcConfigurerAdapter {
+
+
+    //自定义静态资源文件路径
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/zc/resources/**").addResourceLocations("classpath:/static/");
+    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
